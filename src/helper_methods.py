6d5332621837c71c966b110dece885559fd0d681
Danial <46810505+Senejohnny@users.noreply.github.com>
2022-01-25 22:21:10 +0100

    Updated figures based on Tineke feedback

---
+++ src/helper_methods.py
@@ -0,0 +1,201 @@
+from collections import defaultdict
+from typing import  Sequence, Union
+import pandas as pd
+import numpy as np
+from lifelines import KaplanMeierFitter
+from lifelines.statistics import multivariate_logrank_test
+from lifelines.plotting import add_at_risk_counts
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+from statsmodels.iolib.summary2 import summary_col
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
+
+def _get_indexes(df, donor_type, desa_spec:Union[str, Sequence]=None):
+    
+    # donor_type = 'Deceased'
+    desa_spec = set([desa_spec]) if isinstance(desa_spec, str) else desa_spec
+    if 'TypeOfDonor_NOTR' in df.columns:
+        ind_dead = df.TypeOfDonor_NOTR == donor_type 
+    else:
+        ind_dead = df.Donor_Type == donor_type
+
+    ind_desa = df.DESA_Status == 'DESA'
+    ind_T1 = ~ind_desa & ind_dead
+    if desa_spec:
+        ind_desa_spec = df.DESA.apply(lambda x: bool(desa_spec & x))
+        ind_T2 = ind_desa & ~ind_desa_spec & ind_dead
+        ind_T3 = ind_desa & ind_desa_spec & ind_dead
+    else: 
+        ind_ndesa = df['#DESA'].apply(lambda x:  x <= 2) 
+        ind_T2 = ind_desa & ind_ndesa & ind_dead
+        ind_T3 = ind_desa & ~ind_ndesa & ind_dead
+
+    return ind_T1, ind_T2, ind_T3
+
+def create_treatment_grups(data, desa_spec:set=None):
+    df = data.copy(deep=True)
+    desa_spec = set([desa_spec]) if isinstance(desa_spec, str) else desa_spec
+
+    if desa_spec:
+        df = df.assign(
+            No_DESA = df['DESA'].apply(lambda x: 0 if x else 1),
+            Specific_DESA = df['DESA'].apply(lambda x: 1 if x & desa_spec else 0),
+            Other_DESA =  df['DESA'].apply(lambda x: 1 if (x and not x & desa_spec) else 0),
+        )
+        return df.assign(
+            Groups = df[['No_DESA', 'Specific_DESA', 'Other_DESA']].values.argmax(1) + 1
+        )
+    df = df.assign(
+        No_DESA = df['DESA'].apply(lambda x: 0 if x else 1),
+        DESA_12 = df['#DESA'].apply(lambda x: 1 if x <= 2 else 0),
+        DESA_3orMore = df['#DESA'].apply(lambda x: 1 if x >= 3 else 0),
+    )
+    return df.assign(
+        Groups = df[['No_DESA', 'DESA_12', 'DESA_3orMore']].values.argmax(1) + 1
+    )
+
+def feature_scaler(df, scaler='standard'):
+    num_vars = ['RecipientAge_NOTR', 'DonorAge_NOTR', 'CIPHour_DBD', 'CIPHour_DCD']
+    for var in num_vars:
+        try:
+            if scaler == 'standard':
+                mean = df[var].mean()
+                std = df[var].std()
+                df[var] = df[var].apply(lambda x: (x - mean) / std)
+            if scaler == 'maxmin':
+                max, min = df[var].max(), df[var].min()
+                df[var] = df[var].apply(lambda x: (x - min) / (max - min))
+        except KeyError:
+            pass
+    return df
+
+def find_ipw(df, confounders, treatments, verbose=False) -> pd.Series:
+    """ This method finds the inverse probability weights use in causal inference per treatment group """    
+    # scale the features
+    df = feature_scaler(df, scaler='maxmin')
+    
+    # Propensity model
+    formulas = [treatment + ' ~ ' + ' + '.join(confounders) for treatment in treatments]
+    models = [
+        smf.glm(formula=formulas[i], data=df, family=sm.families.Binomial())
+        .fit() for i in range(len(treatments))
+    ]
+
+    # Propensity scores 
+    propensity_scores = np.array([model.fittedvalues for model in models])
+    p = propensity_scores/sum(propensity_scores)
+
+    if verbose:
+        print(summary_col(models))
+        # print('sum propensity score', sum(propensity_scores))
+
+    # Calculate the weights
+    df['w'] = sum([(df[treatment]==1) / p[i] for i, treatment in enumerate(treatments)])
+    return df
+
+def kaplan_meier_curves(df:pd.DataFrame, 
+                        desa_spec:Union[str, Sequence]=None,
+                        donor_type:str='Deceased',
+                        labels:list=None,
+                        adjust=False):
+
+    assert 'Groups' in df.columns, 'Groups column is missing'
+    if 'GraftSurvival10Y_R' in df.columns:
+        failure, time = 'FailureCode10Y_R', 'GraftSurvival10Y_R'
+    else:
+        failure, time = 'Failure', 'Survival[Y]'
+
+    ind_T1, ind_T2, ind_T3 = _get_indexes(df, donor_type=donor_type, desa_spec=desa_spec)
+
+    T = [df.loc[ind_T1, time], df.loc[ind_T2, time], df.loc[ind_T3, time]]
+    E = [df.loc[ind_T1, failure], df.loc[ind_T2, failure], df.loc[ind_T3, failure]]
+    if adjust:
+        assert 'w' in df.columns, 'Weight column is missing'
+        W = [df.loc[ind_T1, 'w'], df.loc[ind_T2, 'w'], df.loc[ind_T3, 'w']]
+    else:
+        W = [None] * 3
+    t = np.linspace(0, 10, 1000)
+    kmfs = [
+        KaplanMeierFitter(label=labels[i])
+        .fit(T[i], event_observed=E[i], timeline=t, weights=W[i]) for i in range(len(T)) 
+    ]
+    p_value = multivariate_logrank_test(
+        df['GraftSurvival10Y_R'], df['Groups'], df['FailureCode10Y_R']
+        ).p_value
+    return kmfs, p_value
+    
+def plot_kaplan_meier_curve(kmfs:list, 
+                            p_value:float=None,
+                            title:str=None,
+                            ci_show:bool=False, 
+                            grid:bool=False, 
+                            add_counts:bool=True,
+                            ax=None,
+                            fontsize:int=15,
+                            legend_size = 10,
+                            xlabel:str='Years after Tranplantation',
+                            ylabel:str='Graft Survival (%)',
+                            verbose:bool=False):
+    """ kmfs: is a list of KaplanMeierFitter instances already fitted """
+
+    colors = ['black', 'blue', 'red']
+    if not ax:
+        fig, ax = plt.subplots(figsize=(8, 7))
+    [kmf.plot(ci_show=ci_show, color=colors[i], ax=ax) for i, kmf in enumerate(kmfs)]
+
+    # Add at-risk, censored, and failure statistics
+    if add_counts:
+        add_at_risk_counts(*kmfs, ax=ax, fontsize=15)
+
+    # add p-value 
+    if p_value:
+        p_value_string = 'p < 0.0001' if p_value < 0.0001 else f'p = {p_value}'
+        ax.add_artist(AnchoredText(p_value_string, loc=4, frameon=False))
+
+    # Other figure settings
+    if title:
+        ax.set_title(title, fontsize=fontsize)
+    ax.set_xlabel(xlabel, fontsize=fontsize)
+    ax.set_ylabel(ylabel, fontsize=fontsize)
+    ax.legend(prop={"size":legend_size})
+    ax.grid(grid)
+    ax.set_ylim(0.2)
+    # plt.tight_layout()
+    # plt.show()
+
+    # Final values
+    if verbose:
+        kmfs_final = [kmf.survival_function_.values[-1][0]*100 for kmf in kmfs]
+        print(f'Top curve: {kmfs_final[0]:.2f}, Middle curve: {kmfs_final[1]: .2f}, Lower curve:{kmfs_final[2]: .2f}')
+        print(f'10-Year Gap is: {kmfs_final[0] - kmfs_final[2] : .2f}')
+        kmfs_100 = [kmf.survival_function_.values[100][0]*100 for kmf in kmfs]
+        print(f'1-Year Gap is: {kmfs_100[1] - kmfs_100[2] : .2f}')
+
+def plot_scatter_diff(dataframe, desa_sets:list, confounders:list, donor_type:str='Deceased', adjust:bool=True, grid:bool=False):
+    time_points = [10, 100, 200, 300, 400, 500, 600, 700, 800, 900, 980]
+    diffs = defaultdict(lambda : defaultdict(list))
+    treatments = ['No_DESA', 'Other_DESA', 'Specific_DESA']
+    for i, desa_set in enumerate(desa_sets):
+        for desa in desa_set:
+            # Compute weighted km curves )
+            df_tret_group = create_treatment_grups(dataframe, desa)
+            df_weight = find_ipw(df_tret_group, confounders, treatments, verbose=False)
+            kmfs, p_value = kaplan_meier_curves(df_weight, desa, donor_type=donor_type, labels=treatments)
+            # Subtract all DESA from Specific DESA
+            diff = kmfs[1].subtract(kmfs[2])
+            diffs[i]['x'].append(diff.index[time_points])
+            diffs[i]['y'].append(diff.values[time_points])
+
+    fig, ax = plt.subplots(figsize=(22, 10))
+    colors = ['red', 'yellow', 'green']
+    labels = [ 'Relevant DESA [Bad]', 'Irrelevant DESA', 'Relevant DESA [Good]']
+    for i in range(len(desa_sets)):
+        ax.scatter(diffs[i]['x'], diffs[i]['y'],  color=colors[i], label=labels[i], linewidth=2.5, alpha= 0.25)
+    plt.axhline(0.1, color='black', linestyle='dashed')
+    plt.axhline(-0.1, color='black', linestyle='dashed')
+    ax.set_xlabel('Years after Tranplantation', fontsize=20)
+    ax.set_ylabel('Diff Graft Survival (%)', fontsize=20)
+    ax.legend(prop={"size":15})
+    ax.grid(grid)
+    plt.tight_layout()
---
+++ src/pipe_store.py
@@ -0,0 +1,310 @@
+import logging
+from functools import wraps
+import datetime as dt
+import pandas as pd
+import numpy as np
+from typing import List, Literal
+from src.constants import (
+    Epitope_DB,
+    RELEVANT_DESA_BAD, 
+    RELEVANT_DESA_GOOD,
+    RELEVANT_DESA_GOOD_OLD,
+)
+from src.utility import get_hla_class, flatten2list, flatten2string, epitopeseq2str, sequence2string
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+def logging(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        tic = dt.datetime.now()
+        result = func(*args, **kwargs)
+        time_taken = str((dt.datetime.now() - tic).total_seconds())
+        logger.info(f"Step: {func.__name__} | Shape: {result.shape} | Computation Time: {time_taken}s")
+        return result
+    return wrapper
+
+@logging
+def data_loading(path):
+    return pd.read_pickle(path)
+
+@logging
+def start_pipeline(dataf:pd.DataFrame, status:str, donor_type:str=None):
+        """ 
+        This function split the data set to 4 categories
+            1. Deceased Donor with antibody epitopes (DESA)
+            2. Deceased Donor without antibody epitopes 
+            3. Living Donor with antibody epitopes
+            4. Living Donor without antibody epitopes
+        If not input is given the whole data frame is used without any split
+
+        Parameters:
+        -----------
+        donor_type: str ['Living', 'Deceased'] 
+            Type of the donor
+        
+        # antibody_epitope: bool
+        #     Presence or absence of antibody epitope
+        """
+        df = dataf.copy()
+        
+        logging_indet = 6
+        logger.info(
+            f'- selceted cohort --> Donor Type: {donor_type}, Epitope Antibody Presence: {status}'
+            .rjust(logging_indet)
+        )
+
+        if not donor_type:
+            ind_dead = df.index
+        else:
+            ind_dead = df.Donor_Type == donor_type
+
+        if status == 'All':
+            ind_desa = True
+        elif status == 'DESA':
+            ind_desa = df.Status == 'DESA'
+        elif status == 'No DESA':
+            ind_desa = df.Status != 'DESA'
+
+        ind = ind_dead & ind_desa
+        
+        return (
+                df[ind].drop(['Status'], axis=1) if 'Status' in df.columns else df[ind].drop(['DESA_Status'], axis=1) 
+                .reset_index(drop=True)
+        )
+
+def keeping_features(df:pd.DataFrame, *cols:str):
+    """ Keeping the columns in the col """
+    
+    return df[list(cols)]
+
+
+from typing import Sequence
+def epitope_preparation(epitopes:Sequence):
+    """ Make sentence from all epitopes in a DB and then joint them in a sentence """
+
+    if isinstance(epitopes, dict):
+        epitopes = set(epitopes.keys())
+    _dict = {db : epitopeseq2str(epitopes & Epitope_DB[db]) for db in ['ABC', 'DR', 'DQ']}
+    return sequence2string(list(_dict.values()))
+
+
+def features_from_antibody_epitopes(df):
+
+    df = df.assign(
+        Relevant_DESA_Bad = df['DESA'].apply(lambda x: int(bool(x & RELEVANT_DESA_BAD))),
+        Relevant_DESA_Good = df['DESA'].apply(lambda x: int(bool(x & RELEVANT_DESA_GOOD))),
+        Class_I = df.EpvsHLA_Donor.apply(
+                    lambda x: int('I' in { get_hla_class(hla.split('*')[0]) for hla in x.values() } ) 
+                ),
+        Class_II = df.EpvsHLA_Donor.apply(
+                    lambda x:  int('II' in { get_hla_class(hla.split('*')[0]) for hla in x.values() } )  
+                ),
+        # Epitopes = df['DESA'].apply(epitope_preparation),
+    )
+    df.drop(['EpvsHLA_Donor'], axis=1, inplace=True)
+    return df
+
+def features_from_mismatch_epitopes(df):
+    df = df.assign(
+        ep_mismatch = df['Epitope_Mismatch'].apply(lambda x: 1 if len(x) else 0),
+        Epitopes = df['Epitope_Mismatch'].apply(epitope_preparation)
+    )
+    return df.drop(['Epitope_Mismatch'], axis=1)
+
+@logging
+def eng_immunological_features(df:pd.DataFrame, antibody_epitope:bool) -> pd.DataFrame:
+    """
+    Engineering immunological features based on the presence of antibody epitopes. In the presence of 
+    antibody epitopes relevance of the eptitopes is important. While in their absence presence or absence of
+    epitope mismatch is important
+
+    Parameters:
+    ----------- 
+    antibody_epitope: bool
+        Presence or absence of antibody epitope
+    """
+
+    if antibody_epitope:
+        return features_from_antibody_epitopes(df)
+    return features_from_mismatch_epitopes(df)
+
+@logging
+def one_hot_encoder(df:pd.DataFrame, col, *cats):
+    """
+    Convert categorical variable into dummy variables and keeps the given categorical columns. 
+    Under the hood uses pandas get_dummies method
+
+    Parameters:
+    -----------
+    col: The specific column to be one hot encoded
+    
+    cats: the categories/columns that will be kept after one hot encoding.
+
+    Example:
+    --------
+    >> s = pd.Series(list('abca'))
+    >> pd.get_dummies(s)
+       a  b  c
+    0  1  0  0
+    1  0  1  0
+    2  0  0  1
+    3  1  0  0
+    """
+    
+    encoded_df = pd.get_dummies(df[col])    
+    df.drop(col, axis=1, inplace=True)
+    if not cats:
+        return pd.concat([df, encoded_df], axis=1)
+    cats = list(cats)    
+    return pd.concat([df, encoded_df[cats]], axis=1)
+
+@logging
+def integer_encoder(df:pd.DataFrame, *cols):
+    """
+    Encode the object as an enumerated type, i.e. Integer Encoding. This function is particularly 
+    used for features with 2 category. Under the hood uses pandas factorize method. For Features 
+    with more categories consider one_hot_encoder.
+
+    Parameters:
+    -----------
+    cols: Name of the column/feature in the dataset
+    
+    Example:
+    --------
+    >> pd.factorize(['b', 'b', 'a', 'c', 'b'])
+    (
+        array([0, 0, 1, 2, 0]...),
+        array(['b', 'a', 'c'], dtype=object)
+    )
+    """
+
+    if not cols:
+        raise ValueError('Name of the dataset column is missing')
+
+    for col in cols:
+        if df[col].dtype == 'object':
+            df[col], _ = pd.factorize(df[col])
+    return df
+
+@logging
+def set_time_event_label(df:pd.DataFrame, E='Failure', T='Survival[Y]'):
+    """ 
+    Set the time and event label to T and E respectively 
+    
+    Parameters:
+    -----------
+    E: The event column in the dataset
+    T: The time column in the dataset
+    """
+
+    df['E'] = df['Failure']
+    df['T'] = df['Survival[Y]']
+    df.drop([E, T], axis=1, inplace=True)
+    return df
+
+@logging
+def censoring_deaths(df:pd.DataFrame):
+    """
+    Transplants where patient dies with functioning graft, i.e. 2 is changed to no failure, i.e. 0
+    Method set_time_event_label should be run beforehand
+    """
+
+    return df.assign(
+        E=df['E'].map({0:0, 1:1, 2:0})
+    )
+
+
+@logging
+def setting_prediction_horizon(df:pd.DataFrame, time:int, E='E', T='T'):
+    """ 
+    Setting the prediction horizon, i.e. keep events for T < year & make event to zero 
+    for T > year. Method set_time_event_label should be run beforehand
+    
+    Parameters:
+    -----------
+    time: in Year
+    E: event column
+    T: survival time
+    """
+
+    ind = df[T].apply(lambda x: x <= time)
+    return  df.assign(
+        E = ind * df[E],
+        T = df[T].apply(lambda x: x if x <= time else time)
+    )
+
+
+def scikit_lifeline_adapter(df:pd.DataFrame):
+    """ Splits the data frame to input matrix and output vector suitable for lifeline sklearn adapter """
+
+    X = df.drop('T', axis=1)
+    y = df.pop('T')
+    return (X, y)
+
+
+def scikit_survival_adapter(df:pd.DataFrame):
+    """ Splits the data frame to input matrix and output vector suitable for scikit survival """
+
+    X = df.drop(['E', 'T'], axis=1)
+    y = np.array(list(zip(df['E'], df['T'])), np.dtype('bool, float'))
+    return (X, y)
+
+
+def classification_adapter(df:pd.DataFrame, time:float=1, drop_censored=True):
+    """ Transforms the survival analysis problem into a classification problem by creating a lable
+    column L where
+        - 1 denotes failed transplants within time horizon T
+        - 0 denotes not-failed transplants within time horizon T  
+        - 99 denotes censored transplants within time horizon T 
+
+    Parameters:
+    -----------
+    df: dataframe
+    time: Prediction horizon in Year
+    drop_censored: Bool
+        Drop the censored transplats denoted by 2 
+    """
+
+    df['L'] =  df[['E', 'T']].apply(lambda x: 
+                    1 if (x[0] == 1) & (x[1] <= time) else 99 if (x[0] == 0) & (x[1] < time) else 0, 
+                    axis=1,
+                )
+    if drop_censored:
+        df = df[df.L != 99]
+    X = df.drop(['E', 'T', 'L'], axis=1)
+    y = df['L']
+    return (X, y)
+
+def feature_scaler(df, cols:list, scaler:Literal['standard', 'maxmin']='standard'):
+    """ 
+    Scales the given data frame features based on the selected scaler
+
+    Parameters:
+    -----------
+    scaler: str
+    Options are standard & maxmin
+    """
+    for var in cols:
+        try:
+            if scaler == 'standard':
+                mean = df[var].mean()
+                std = df[var].std()
+                df[var] = df[var].apply(lambda x: (x - mean) / std)
+            if scaler == 'maxmin':
+                max, min = df[var].max(), df[var].min()
+                df[var] = df[var].apply(lambda x: (x - min) / (max - min))
+        except KeyError:
+            print(f'Variable {var} does not exist in the data frame columns')
+            pass
+    return df
+
+def dataframe_to_dataset(X, y):
+    import tensorflow as tf
+    
+    X, y = X.copy(), y.copy()
+    ds = tf.data.Dataset.from_tensor_slices((dict(X), y))
+    ds = ds.shuffle(buffer_size=len(X))
+    return ds
